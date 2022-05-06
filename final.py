777cb36764016612493c2c247b6a90bc8898df06
Ana Collados <89938385+AnaCollados@users.noreply.github.com>
2022-05-06 10:34:12 +0200

    Update final.py

--- final.py
+++ final.py
@@ -262,5 +262,5 @@ if selected == "Contact":
         with open(file_name) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-    local_css("style/style_form.css")
+    local_css("style_form.css")
 
